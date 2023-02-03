44f2904e4b39369ef3e85fd1eb92ffea363a419b
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-03 13:47:33 +0000

    Iteration 4

--- .ccls-cache/@home@runner@U1L2P5Gombos/main.c
+++ .ccls-cache/@home@runner@U1L2P5Gombos/main.c
@@ -7,21 +7,20 @@
 
 int main(void) { //declares main function
   int feet, inches, height; //initialize variables as integer
+
+  printf("This program allows you to convert your height from feet and inches to inches only. You will get two different prompts to input your height in whole numbers:\n\t- Feet: Input your height in feet.\n\t- Inches: Input the remainder of inches.\nExample: If your height is 5\'6\":\n\t- Feet: 5\n\t- Inches: 6"); //instructions
   
-  printf("How tall are you in feet only: ");
+  printf("\n\nFeet: ");
   //asks how tall you are in feet
   scanf("%d", &feet); //accepts user input and formats as integer
-  printf("How tall are you in inches only: ");
+  printf("Inches: ");
   //asks how tall you are in inches
   scanf("%d", &inches); //accepts user input and formats it as integer
   
   height = feet*12; //converts feet to inches
   height += inches; //adds the inches
-  
-  printf("Feet: %d", feet); //prints feet
-  printf("\nInches %d", inches); //prints inches
 
-  printf("\nYour total height in inches is: %d inches", height);
+  printf("\nYour height of %d\'%d\" is equal to: %d inches", feet, inches, height);
   //^outputs the height in inches
   
   return 0;
