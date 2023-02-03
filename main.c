6f003c3bcfadd95e688d97f598bcf8cf177bc01c
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-03 13:22:49 +0000

    Iteration 1

--- main.c
+++ main.c
@@ -3,9 +3,20 @@
   Height in Inches
   Max Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //imports input and output functions
 
-int main(void) {
+int main(void) { //declares main function
+  int feet, inches, height; //initialize variables as integer
+  
+  printf("How tall are you in feet only: ");
+  //asks how tall you are in feet
+  scanf("%d", &feet); //accepts user input and formats as integer
+  printf("How tall are you in inches only: ");
+  //asks how tall you are in inches
+  scanf("%d", &inches); //accepts user input and formats it as integer
+  
+  printf("Feet: %d", feet); //prints feet
+  printf("\nInches %d", inches); //prints inches
   
   return 0;
 }
